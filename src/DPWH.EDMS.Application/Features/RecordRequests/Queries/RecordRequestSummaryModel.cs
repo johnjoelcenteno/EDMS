515edb35266858fcb348f3ce52af375d8ea410d0
mel@blackfortconsulting.com <mel@blackfortconsulting.com>
2024-06-21 11:47:54 +0800

    Fix ControlNumber property #10081

--- src/DPWH.EDMS.Application/Features/RecordRequests/Queries/RecordRequestSummaryModel.cs
+++ src/DPWH.EDMS.Application/Features/RecordRequests/Queries/RecordRequestSummaryModel.cs
@@ -2,7 +2,7 @@
 public record RecordRequestSummaryModel
 {
     public Guid Id { get; set; }
-    public string ControlNumber { get; set; }
+    public int ControlNumber { get; set; }
     public string ClaimantType { get; set; }
     public DateTimeOffset DateRequested { get; set; }        
     public string Purpose { get; set; }
