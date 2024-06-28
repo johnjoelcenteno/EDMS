4d6e9838c37dfa7b9cc788685b6a4b770e5f1b51
Andrei Joshua Valencia <andreijoshua.valencia@blackfort.ph>
2024-06-28 15:06:24 +0800

    Remove Inspector and Requestor Fixed where clause

--- src/DPWH.EDMS.IDP.Core/Constants/ApplicationPolicies.cs
+++ src/DPWH.EDMS.IDP.Core/Constants/ApplicationPolicies.cs
@@ -5,9 +5,9 @@ using static ApplicationRoles;
 public static class ApplicationPolicies
 {
     public static string[] AdminOnly => new[] { SystemAdmin, SuperAdmin };
-    public static string[] RequireActiveRoles => new[] { SystemAdmin, SuperAdmin, Manager, EndUser, ITSupport, Inspector, Requestor };
+    public static string[] RequireActiveRoles => new[] { SystemAdmin, SuperAdmin, Manager, EndUser, ITSupport };
     public static string[] RequireForUserManagement => new[] { SystemAdmin, SuperAdmin, ITSupport };
-    public static readonly string[] NoLicenseUsers = { SystemAdmin, Inspector, Requestor, EndUser };
+    public static readonly string[] NoLicenseUsers = { SystemAdmin, EndUser };
 
     public static readonly IReadOnlyDictionary<string, string[]> GetAll = new Dictionary<string, string[]>{
         { nameof(AdminOnly), AdminOnly },
