c423a14855a5cf7402b37a66bb3a37d9130181f1
Andrei Joshua Valencia <andreijoshua.valencia@blackfort.ph>
2024-06-28 13:34:59 +0800

    Filter users when counting license - End users dont consume a license

--- src/DPWH.EDMS.IDP.Core/Constants/ApplicationPolicies.cs
+++ src/DPWH.EDMS.IDP.Core/Constants/ApplicationPolicies.cs
@@ -7,7 +7,7 @@ public static class ApplicationPolicies
     public static string[] AdminOnly => new[] { SystemAdmin, SuperAdmin };
     public static string[] RequireActiveRoles => new[] { SystemAdmin, SuperAdmin, Manager, EndUser, ITSupport, Inspector, Requestor };
     public static string[] RequireForUserManagement => new[] { SystemAdmin, SuperAdmin, ITSupport };
-    public static readonly string[] NoLicenseUsers = { SystemAdmin, Inspector, Requestor };
+    public static readonly string[] NoLicenseUsers = { SystemAdmin, Inspector, Requestor, EndUser };
 
     public static readonly IReadOnlyDictionary<string, string[]> GetAll = new Dictionary<string, string[]>{
         { nameof(AdminOnly), AdminOnly },
