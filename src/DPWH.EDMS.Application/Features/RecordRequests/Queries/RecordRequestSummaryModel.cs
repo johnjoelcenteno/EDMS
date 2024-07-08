642e6f28a9aa15896e819ace35426cbf39dd3674
Arishavelle Karl Villanueva <59736975+arishavelle18@users.noreply.github.com>
2024-07-08 11:26:00 +0800

    -DateRequested change to nullable DateTimeOffset

--- src/DPWH.EDMS.Application/Features/RecordRequests/Queries/RecordRequestSummaryModel.cs
+++ src/DPWH.EDMS.Application/Features/RecordRequests/Queries/RecordRequestSummaryModel.cs
@@ -4,7 +4,7 @@ public record RecordRequestSummaryModel
     public Guid Id { get; set; }
     public int ControlNumber { get; set; }
     public string ClaimantType { get; set; }
-    public DateTimeOffset DateRequested { get; set; }
+    public DateTimeOffset? DateRequested { get; set; }
     public string Purpose { get; set; }
     public string Status { get; set; }
     public IEnumerable<RequestedRecordModel> RequestedRecords { get; set; } = [];
