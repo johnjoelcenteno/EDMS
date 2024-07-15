b88ebdce3e4fb5b544f84512e6f17bb8bbfe6cb5
Arishavelle Karl Villanueva <59736975+arishavelle18@users.noreply.github.com>
2024-07-15 12:48:17 +0800

    - Refactor PersonalRecord, DPWH Issuance, Perosnal Records

---
+++ src/DPWH.EDMS.Web/Client/Pages/DataLibrary/RecordTypes/Common/Model/RecordsLibraryModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DPWH.EDMS.Web.Client.Pages.DataLibrary.RecordTypes.Common.Model;
+
+public class RecordsLibraryModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Section { get; set; }
+    public string Category { get; set; } = "System";
+    public string Office { get; set; }
+    public bool IsActive { get; set; }
+}
+
