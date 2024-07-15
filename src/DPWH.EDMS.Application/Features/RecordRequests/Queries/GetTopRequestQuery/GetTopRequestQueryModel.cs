b89a024dc60c64dc58b7b3dfb6235d67f153946d
Arishavelle Karl Villanueva <59736975+arishavelle18@users.noreply.github.com>
2024-07-15 16:29:44 +0800

    [API] Add Get Top 10 Requests endpoint  in request Management

---
+++ src/DPWH.EDMS.Application/Features/RecordRequests/Queries/GetTopRequestQuery/GetTopRequestQueryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPWH.EDMS.Application.Features.RecordRequests.Queries.GetTopRequestQuery;
+
+public class GetTopRequestQueryModel
+{
+    public string RecordName { get; set; }
+    public long Total {  get; set; }
+}
