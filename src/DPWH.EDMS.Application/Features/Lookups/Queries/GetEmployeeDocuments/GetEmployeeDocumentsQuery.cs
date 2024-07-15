3bc3fdf354f2874d9905dd03f5fae327b377543e
mel@blackfortconsulting.com <mel@blackfortconsulting.com>
2024-07-15 19:03:09 +0800

    Update lookup, new name is Employee Documents. This is the archives in globodox #10554

---
+++ src/DPWH.EDMS.Application/Features/Lookups/Queries/GetEmployeeDocuments/GetEmployeeDocumentsQuery.cs
@@ -0,0 +1,23 @@
+using DPWH.EDMS.Application.Contracts.Persistence;
+using DPWH.EDMS.Application.Features.RecordTypes;
+using DPWH.EDMS.Domain.Extensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DPWH.EDMS.Application.Features.Lookups.Queries.GetEmployeeDocuments;
+
+public record GetEmployeeDocumentsQuery : IRequest<IEnumerable<GetLookupResult>>;
+
+internal sealed class GetEmployeeDocumentsQueryHandler(IReadRepository repository) : IRequestHandler<GetEmployeeDocumentsQuery, IEnumerable<GetLookupResult>>
+{
+    public async Task<IEnumerable<GetLookupResult>> Handle(GetEmployeeDocumentsQuery request, CancellationToken cancellationToken)
+    {
+        var category = EnumExtensions.GetDescriptionFromValue<RecordTypesCategory>(RecordTypesCategory.EmployeeDocuments.ToString());
+        var recordTypes = await repository.RecordTypesView
+            .Where(d => d.Category == category)
+            .Select(d => new GetLookupResult(d.Id, d.Name))
+            .ToListAsync(cancellationToken);
+
+        return recordTypes.OrderBy(r => r.Name);
+    }
+}
--- src/DPWH.EDMS.Application/Features/RecordTypes/RecordTypesCategory.cs
+++ src/DPWH.EDMS.Application/Features/RecordTypes/RecordTypesCategory.cs
@@ -3,10 +3,11 @@ using System.ComponentModel;
 namespace DPWH.EDMS.Application.Features.RecordTypes;
 
 public enum RecordTypesCategory
-{
-    Archived,
+{    
     [Description("DPWH Issuances")]
     Issuances,
     [Description("Employee Records")]
-    EmployeeRecords
+    EmployeeRecords,
+    [Description("Employee Documents")]
+    EmployeeDocuments
 }
