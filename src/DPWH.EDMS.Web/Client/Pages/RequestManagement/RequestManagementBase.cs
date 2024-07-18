22073b3f30a02a5f38a892a56c48832cb438a6af
Leonardo Ganas Jr <leonardo.ganas@blackfort.ph>
2024-07-18 15:56:41 +0800

    Refactor Grid Filter To Fix Conflict In Dynamic GridBase

--- src/DPWH.EDMS.Web/Client/Pages/RequestManagement/RequestManagementBase.cs
+++ src/DPWH.EDMS.Web/Client/Pages/RequestManagement/RequestManagementBase.cs
@@ -1,11 +1,26 @@
-﻿using DPWH.EDMS.Client.Shared.Models;
+﻿using DPWH.EDMS.Api.Contracts;
+using DPWH.EDMS.Client.Shared.MockModels;
+using DPWH.EDMS.Client.Shared.Models;
+using DPWH.EDMS.Components.Helpers;
 using DPWH.EDMS.Web.Client.Shared.RecordRequest.Grid;
 using Telerik.Blazor.Components;
+using Telerik.DataSource;
 
 namespace DPWH.EDMS.Web.Client.Pages.RequestManagement;
 
 public class RequestManagementBase : RecordRequestGridComponentBase
 {
+    protected DateTime? SelectedDate { get; set; }
+    protected int? SearchControlNumber { get; set; }
+    protected string? SearchFullName { get; set; }
+    protected string? SearchPurpose { get; set; }
+    protected string? SearchStatus { get; set; }
+    protected List<string> StatusList = new List<string>
+    {
+        "Review",
+        "Release",
+        "Claimed"
+    };
     protected async override Task OnInitializedAsync()
     {
         await HandleOnLoadGrid();
@@ -29,4 +44,39 @@ public class RequestManagementBase : RecordRequestGridComponentBase
     {
         HandleSelectedItemOverview(args, "request-management/view-request-form/");
     }
+
+    protected async void SetFilterGrid()
+    {
+        var filters = new List<Api.Contracts.Filter>();
+
+        AddDateFilter(filters);
+        AddTextSearchFilterIfNotNull(filters, nameof(RecordRequestModel.ControlNumber), SearchControlNumber?.ToString(), "eq");
+        AddTextSearchFilterIfNotNull(filters, nameof(RecordRequestModel.FullName), SearchFullName, "contains");
+        AddTextSearchFilterIfNotNull(filters, nameof(RecordRequestModel.Status), SearchStatus, "contains");
+        AddTextSearchFilterIfNotNull(filters, nameof(RecordRequestModel.Purpose), SearchPurpose, "contains");
+
+        SearchFilterRequest.Logic = DataSourceHelper.AND_LOGIC;
+        SearchFilterRequest.Filters = filters.Any() ? filters : null;
+
+        await LoadData();
+        StateHasChanged();
+    }
+
+    private void AddDateFilter(List<Api.Contracts.Filter> filters)
+    {
+        if (SelectedDate.HasValue)
+        {
+            AddTextSearchFilter(filters, nameof(RecordRequestModel.DateRequested), SelectedDate.Value.ToString(), "gte");
+            AddTextSearchFilter(filters, nameof(RecordRequestModel.DateRequested), SelectedDate.Value.AddDays(1).ToString(), "lte");
+        }
+    }
+
+    private void AddTextSearchFilterIfNotNull(List<Api.Contracts.Filter> filters, string fieldName, string? value, string operation)
+    {
+        if (!string.IsNullOrEmpty(value))
+        {
+            AddTextSearchFilter(filters, fieldName, value, operation);
+        }
+    }
+
 }
