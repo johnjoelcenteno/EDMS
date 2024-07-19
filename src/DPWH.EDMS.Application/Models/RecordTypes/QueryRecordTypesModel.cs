d78dae37127e072c350edf407d148a423c39e26b
Arishavelle Karl Villanueva <59736975+arishavelle18@users.noreply.github.com>
2024-07-19 16:17:39 +0800

    fix build errorr

--- src/DPWH.EDMS.Application/Models/RecordTypes/QueryRecordTypesModel.cs
+++ src/DPWH.EDMS.Application/Models/RecordTypes/QueryRecordTypesModel.cs
@@ -5,8 +5,9 @@ public class QueryRecordTypesModel
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Category { get; set; }
-    public string Section { get; set; }
-    public string Office { get; set; }
+    public string? Section { get; set; }
+    public string? Office { get; set; }
+    public string? Code {  get; set; }
     public bool IsActive { get; set; }
     public DateTimeOffset Created { get; set; }
     public string CreatedBy { get; set; }
