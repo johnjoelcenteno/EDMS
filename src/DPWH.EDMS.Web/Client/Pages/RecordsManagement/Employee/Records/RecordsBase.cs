32eb0e1d322bb87b668dda654565eb6d40e0b355
christian.decembrana.blackfort.ph <christian.decembrana@blackfort.ph>
2024-07-22 14:14:55 +0800

    record mgmt refactor + restructure

--- src/DPWH.EDMS.Web/Client/Pages/RecordsManagement/Employee/Records/RecordsBase.cs
+++ src/DPWH.EDMS.Web/Client/Pages/RecordsManagement/Employee/Records/RecordsBase.cs
@@ -1,5 +1,34 @@
-﻿namespace DPWH.EDMS.Web.Client.Pages.RecordsManagement.Employee.Records;
+﻿using DPWH.EDMS.Api.Contracts;
+using DPWH.EDMS.Web.Client.Shared.Records.Grid;
+using Microsoft.AspNetCore.Components;
+using Telerik.Blazor.Components;
 
-public class RecordsBase
+namespace DPWH.EDMS.Web.Client.Pages.RecordsManagement.Employee.Records;
+
+public class RecordsBase : RecordsGridComponentBase
 {
+    [Parameter] public required GetUserByIdResult SelectedEmployee { get; set; }
+
+    protected async override Task OnInitializedAsync()
+    {
+        IsLoading = true;
+        EmployeeId = SelectedEmployee.EmployeeId;
+        await HandleOnLoadGrid();
+        IsLoading = false;
+    }
+
+    protected override void OnInitialized()
+    {
+        // ----
+    }
+
+    //protected void GoToAddNewRequest()
+    //{
+    //    HandleGoToAddNewRequest("record-management/request-history/add/" + SelectedEmployee.Id);
+    //}
+
+    protected void GoToSelectedItemOverview(GridRowClickEventArgs args)
+    {
+        //HandleSelectedItemOverview(args, "request-management/view-request-form/");
+    }
 }
---
+++ src/DPWH.EDMS.Web/Client/Shared/Records/Grid/RecordsGridComponentBase.cs
@@ -0,0 +1,137 @@
+﻿using DPWH.EDMS.Api.Contracts;
+using DPWH.EDMS.Client.Shared.APIClient.Services.RecordManagement;
+using DPWH.EDMS.Client.Shared.Enums;
+using DPWH.EDMS.Components.Components.ReusableGrid;
+using DPWH.EDMS.Components.Helpers;
+using Microsoft.AspNetCore.Components;
+using Telerik.Blazor.Components;
+
+namespace DPWH.EDMS.Web.Client.Shared.Records.Grid;
+
+public class RecordsGridComponentBase : GridBase<RecordModel>
+{
+    [Inject] public required IRecordManagementService RecordManagementService { get; set; }
+
+    protected string EmployeeId { get; set; } = string.Empty;
+    protected int ActiveTabIndex { get; set; } = 1;
+    protected HashSet<string> RequestStates = new HashSet<string>();
+
+    protected async Task HandleOnLoadGrid()
+    {
+        //LoadRequestStates();
+        ServiceCb = RecordManagementService.Query;
+        await LoadData();
+    }
+
+    //protected void HandleGoToAddNewRequest(string uri)
+    //{
+    //    NavManager.NavigateTo(uri);
+    //}
+
+    protected void HandleSelectedItemOverview(GridRowClickEventArgs args, string uri)
+    {
+        IsLoading = true;
+
+        var selectedItem = args.Item as RecordRequestModel;
+
+        if (selectedItem != null)
+        {
+            NavManager.NavigateTo(uri + selectedItem.Id.ToString());
+        }
+
+        IsLoading = false;
+    }
+    //protected void LoadRequestStates()
+    //{
+    //    RequestStates = new HashSet<string>(
+    //        Enum.GetValues(typeof(RecordRequestTabStates))
+    //            .Cast<RecordRequestTabStates>()
+    //            .Select(e => e.ToString())
+    //    );
+    //}
+    //protected async Task TabChangedHandler(int newIndex)
+    //{
+    //    ActiveTabIndex = newIndex;
+    //    var filters = new List<Api.Contracts.Filter>();
+    //    string status = Enum.GetName(typeof(RecordRequestTabStates), ActiveTabIndex)!;
+
+    //    if (ActiveTabIndex != 0 && !string.IsNullOrEmpty(status))
+    //    {
+    //        AddTextSearchFilter(filters, nameof(RecordRequestModel.Status), status);
+    //    }
+
+    //    // Set the filters
+    //    SearchFilterRequest.Logic = DataSourceHelper.AND_LOGIC;
+    //    SearchFilterRequest.Filters = filters;
+
+    //    // Load data with the updated filters
+    //    await LoadData();
+    //    StateHasChanged();
+    //}
+
+    protected override async Task LoadData(bool bPageChanged = false)
+    {
+        IsLoading = true;
+
+        // Set the number of items to retrieve
+        DataSourceReq.Take = PageSize;
+
+        // Calculate the number of items to skip based on the current page
+        if (bPageChanged)
+        {
+            DataSourceReq.Skip = (Page - 1) * PageSize;
+        }
+        else
+        {
+            Page = 1;
+            DataSourceReq.Skip = 0;
+        }
+
+        // Call the method to construct the filter requests
+        GetFilterRequests();
+
+        try
+        {
+            var result = new DataSourceResult();
+
+            // Retrieve data from the BookingService based on the filter requests
+            if (!string.IsNullOrEmpty(EmployeeId))
+            {
+                result = await RecordManagementService.QueryByEmployeeId(EmployeeId, DataSourceReq);
+            }
+            else
+            {
+                result = await RecordManagementService.Query(DataSourceReq);
+            }
+
+            // Convert the retrieved data to a list of BookingModel objects
+            GridData = GenericHelper.GetListByDataSource<RecordModel>(result.Data);
+
+            // Set the total number of items for pagination purposes
+            TotalItems = result.Total;
+        }
+        catch (Exception ex) when (ex is ApiException<ProblemDetails> apiExtension)
+        {
+            var problemDetails = apiExtension.Result;
+            var error = problemDetails.AdditionalProperties.ContainsKey("error") ? problemDetails.AdditionalProperties["error"].ToString() : problemDetails.AdditionalProperties["errors"].ToString();
+            ToastService.ShowError(error);
+
+            if (problemDetails.Status == 401)
+                NavManager.NavigateTo("/logout", true);
+        }
+        catch (Exception ex) when (ex is ApiException apiExt)
+        {
+            var htmlContent = new RenderFragment(builder =>
+            {
+                builder.AddMarkupContent(0, apiExt.Message);
+            });
+            ToastService.ShowError(htmlContent);
+
+            if (apiExt.StatusCode == 401)
+                NavManager.NavigateTo("/logout", true);
+        }
+
+        // Loading is complete
+        IsLoading = false;
+    }
+}
