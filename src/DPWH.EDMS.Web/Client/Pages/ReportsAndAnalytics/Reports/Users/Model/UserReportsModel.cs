bf81d1fc93c118466c76038f3f7469d6101d6a6e
Arishavelle Karl Villanueva <59736975+arishavelle18@users.noreply.github.com>
2024-07-24 11:10:41 +0800

    User Report

---
+++ src/DPWH.EDMS.Web/Client/Pages/ReportsAndAnalytics/Reports/Users/Model/UserReportsModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DPWH.EDMS.Web.Client.Pages.ReportsAndAnalytics.Reports.Users.Model;
+
+public class UserReportsModel
+{
+    public string? UserAccess { get; set; }
+    public string? Position { get; set; } // Added field
+    public string? Email { get; set; } // Added field
+    public string? SubOffice { get; set; } // Added field
+    public string? Office { get; set; } // Added field
+    public DateTimeOffset? CreatedDate { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
