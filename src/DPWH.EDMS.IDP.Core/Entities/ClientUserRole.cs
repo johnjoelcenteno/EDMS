d206f355017a16e9758b4d1a18160448beb4d737
mel@blackfortconsulting.com <mel@blackfortconsulting.com>
2024-07-25 21:46:22 +0800

    Refactor license status #10743

---
+++ src/DPWH.EDMS.IDP.Core/Entities/ClientUserRole.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DPWH.EDMS.IDP.Core.Entities;
+
+[Table("ClientUserRoles")]
+public class ClientUserRole
+{
+    private ClientUserRole()
+    {
+
+    }
+
+    public static ClientUserRole Create(string userId, ClientRole clientRole)
+    {
+        return new ClientUserRole
+        {
+            ClientRoleId = clientRole.Id,
+            UserId = userId
+        };
+    }
+
+    [Key]
+    public Guid Id { get; protected set; }
+
+    /// <summary>
+    /// This mapped to Id of clientRole in [ClientRoles] table
+    /// </summary>
+    [ForeignKey(nameof(ClientRole))]
+    public Guid ClientRoleId { get; set; }
+
+    /// <summary>
+    /// This mapped to user in [AspNetUsers] table
+    /// </summary>
+    public string UserId { get; set; }
+
+    public virtual ClientRole ClientRole { get; set; }
+
+    [ForeignKey(nameof(UserId))]
+    public virtual ApplicationUser User { get; set; }
+}
