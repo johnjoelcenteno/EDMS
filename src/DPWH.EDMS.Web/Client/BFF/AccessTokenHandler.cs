26a68c29fb5913cfe64d4f72925413f063737f48
christian.decembrana.blackfort.ph <christian.decembrana@blackfort.ph>
2024-07-30 17:43:36 +0800

    ongoing nav manager

--- src/DPWH.EDMS.Web/Client/BFF/AccessTokenHandler.cs
+++ src/DPWH.EDMS.Web/Client/BFF/AccessTokenHandler.cs
@@ -38,19 +38,19 @@ public class AccessTokenHandler : DelegatingHandler
                 }
                 else
                 {
-                    _navManager.NavigateTo("/401", true);
+                    //_navManager.NavigateTo("/401", true);
                 }
             }
-            else
-            {
-                _navManager.NavigateTo("/401", true);
-            }
+            //else
+            //{
+            //    _navManager.NavigateTo("/401", true);
+            //}
 
         }
         catch (Exception ex)
         {
             //_logger.LogError(ex.Message, ex);
-            _navManager.NavigateTo("/401", true);
+            //_navManager.NavigateTo("/401", true);
         }
 
         return await base.SendAsync(request, cancellationToken);
