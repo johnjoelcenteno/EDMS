65f1fd149107cf100d0aa34268fef92438453349
Hennison <hennison.dee@blackfort.ph>
2024-08-05 23:20:34 +0800

    Add expandable list for my requests

--- src/DPWH.EDMS.Web/Client/Pages/CurrentUser/Records/RecordsBase.cs
+++ src/DPWH.EDMS.Web/Client/Pages/CurrentUser/Records/RecordsBase.cs
@@ -1,29 +1,38 @@
 ﻿using DPWH.EDMS.Api.Contracts;
 using DPWH.EDMS.Client.Shared.APIClient.Services.Lookups;
+using DPWH.EDMS.Client.Shared.APIClient.Services.RecordManagement;
 using DPWH.EDMS.Client.Shared.MockModels;
 using DPWH.EDMS.Client.Shared.Models;
 using DPWH.EDMS.Components.Components.ReusableGrid;
 using DPWH.EDMS.Components.Helpers;
 using DPWH.EDMS.IDP.Core.Constants;
+using DPWH.EDMS.Web.Client.Pages.DataLibrary.RecordTypes.Common.Model;
+using DPWH.EDMS.Web.Client.Shared.Services.ExceptionHandler;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Telerik.Blazor.Components;
+using Telerik.Blazor.Components.Common.Trees.Models;
 
 namespace DPWH.EDMS.Web.Client.Pages.CurrentUser.Records;
 
-public class RecordsBase : GridBase<GetLookupResult>
+public class RecordsBase : GridBase<LookupRecordModels>
 {
     [CascadingParameter] private Task<AuthenticationState>? AuthenticationStateAsync { get; set; }
     [Parameter] public required string Id { get; set; }
     [Inject] public required NavigationManager NavigationManager { get; set; }
     [Inject] public required ILookupsService LookupsService { get; set; }
+    [Inject] public required IRecordManagementService RecordManagementService { get; set; }
     protected EDMS.Client.Shared.MockModels.RecordModel Record { get; set; }
     protected MockCurrentData CurrentData { get; set; }
+    protected List<LookupRecordModels> GetRecordType = new List<LookupRecordModels>();
     protected List<Document> DocumentList = new List<Document>();
+    public IEnumerable<TreeItem> Data { get; set; }
+    public IEnumerable<object> ExpandedItems { get; set; }
     protected GetLookupResultIEnumerableBaseApiResponse GetEmployeeRecords { get; set; } = new GetLookupResultIEnumerableBaseApiResponse();
 
     protected string DisplayName = "---";
     protected string Role = string.Empty;
+    protected string EmployeeId;
     protected override void OnInitialized()
     {
         BreadcrumbItems.Add(new BreadcrumbModel
@@ -40,13 +49,30 @@ public class RecordsBase : GridBase<GetLookupResult>
     protected async override Task OnInitializedAsync()
     {
         IsLoading = true;
-
-        await GetDocumentRecords();
         await GetUserInfo();
-
+        await GetDocumentRecordsDetails();
         IsLoading = false;
     }
 
+    public async Task OnRowClickHandler(GridRowClickEventArgs args)
+    {
+        var currItem = args.Item as LookupRecordModels;
+
+        var state = GridRef.GetState();
+
+        bool isCurrItemExpanded = state.ExpandedItems.Any(x => x.Id == currItem?.Id);
+
+        if (isCurrItemExpanded)
+        {
+            state.ExpandedItems.Remove(currItem!);
+        }
+        else
+        {
+            state.ExpandedItems.Add(currItem!);
+        }
+
+        await GridRef.SetStateAsync(state);
+    }
     private async Task GetUserInfo()
     {
         if (AuthenticationStateAsync is null)
@@ -59,6 +85,7 @@ public class RecordsBase : GridBase<GetLookupResult>
         if (user.Identity is not null && user.Identity.IsAuthenticated)
         {
             var roleValue = user.Claims.FirstOrDefault(c => c.Type == "role")!.Value;
+            EmployeeId = user.Claims.FirstOrDefault(c => c.Type == "employee_number")!.Value;
             DisplayName = !string.IsNullOrEmpty(user.Identity.Name) ? GenericHelper.CapitalizeFirstLetter(user.Identity.Name) : "---";
             Role = GetRoleLabel(roleValue);
 
@@ -70,23 +97,55 @@ public class RecordsBase : GridBase<GetLookupResult>
         return ApplicationRoles.GetDisplayRoleName(roleValue, "Unknown Role");
     }
 
-    protected async Task GetDocumentRecords()
+
+    protected async Task GetDocumentRecordsDetails()
     {
         IsLoading = true;
+        ServiceCb = RecordManagementService.Query;
+        var filters = new List<Api.Contracts.Filter>();
+        AddTextSearchFilterIfNotNull(filters, nameof(LookupRecordModels.EmployeeId), EmployeeId, "eq");
+        SearchFilterRequest.Logic = DataSourceHelper.AND_LOGIC;
+        SearchFilterRequest.Filters = filters.Any() ? filters : null;
+        await LoadData();
 
         var res = await LookupsService.GetPersonalRecords();
         if (res.Success)
         {
             GetEmployeeRecords = res;
+            var convertedData = GetEmployeeRecords.Data
+                    .Select(item => new LookupRecordModels
+                    {
+                        Id = item.Id,
+                        RecordName = item.Name,
+                        Documents = GridData.Where(items => items.RecordName == item.Name).Select(items => new RecordModels
+                        {
+                            Id = items.Id,
+                            RecordName = items.RecordName,
+                            RecordTypeId = items.RecordTypeId,
+                            RecordUri = items.RecordUri,
+                            DocVersion = items.DocVersion,
+                        }).ToList(),
+                     
+                    }).ToList();
+            GetRecordType = convertedData;
         }
 
         IsLoading = false;
     }
+
     public async Task viewData(GridCommandEventArgs args)
     {
-        GetLookupResult selectedId = args.Item as GetLookupResult;
+        RecordModels? selectedId = args.Item as RecordModels;
 
         //Int32.TryParse(samp, out sampNumber);
+        Console.WriteLine(selectedId?.Id);
         NavigationManager.NavigateTo($"/my-records/{selectedId.Id}");
+    }
+    private void AddTextSearchFilterIfNotNull(List<Api.Contracts.Filter> filters, string fieldName, string? value, string operation)
+    {
+        if (!string.IsNullOrEmpty(value))
+        {
+            AddTextSearchFilter(filters, fieldName, value, operation);
+        }
     }
 }
--- src/DPWH.EDMS.Web/Client/Pages/CurrentUser/Records/RecordsPersonalData.cs
+++ src/DPWH.EDMS.Web/Client/Pages/CurrentUser/Records/RecordsPersonalData.cs
@@ -1,20 +1,26 @@
-﻿using DPWH.EDMS.Client.Shared.APIClient.Services.Lookups;
+﻿using AutoMapper;
+using DPWH.EDMS.Api.Contracts;
+using DPWH.EDMS.Client.Shared.APIClient.Services.Lookups;
+using DPWH.EDMS.Client.Shared.APIClient.Services.RecordManagement;
 using DPWH.EDMS.Client.Shared.MockModels;
 using DPWH.EDMS.Client.Shared.Models;
 using DPWH.EDMS.Components.Components.ReusableGrid;
+using DPWH.EDMS.Components.Helpers;
 using Microsoft.AspNetCore.Components;
 
 namespace DPWH.EDMS.Web.Client.Pages.CurrentUser.Records
 {
-    public class RecordsPersonalData : GridBase<PersonalRecordDocument>
+    public class RecordsPersonalData : GridBase<LookupRecordModels>
     {
         [Parameter]
         public string Id { get; set; }
         [Inject] public required ILookupsService LookupsService { get; set; }
-        protected RecordModel CurrentRecord = new RecordModel();
-        protected Api.Contracts.GetLookupResult EmployeeRecord { get; set; } = new Api.Contracts.GetLookupResult();
+        [Inject] public required IRecordManagementService RecordManagementService { get; set; }
+        [Inject] public required IMapper Mapper { get; set; }
+        protected LookupRecordModels EmployeeRecord { get; set; } = new LookupRecordModels();
         protected PersonalRecordDocument Record { get; set; } = new PersonalRecordDocument();
         protected Document Document { get; set; }
+        protected DataSourceRequest body { get; set; } = new DataSourceRequest();
         protected async override Task OnInitializedAsync()
         {
             IsLoading = true;
@@ -30,7 +36,7 @@ namespace DPWH.EDMS.Web.Client.Pages.CurrentUser.Records
             BreadcrumbItems.Add(new BreadcrumbModel
             {
                 Icon = "menu",
-                Text = EmployeeRecord.Name,
+                Text = EmployeeRecord.RecordName,
                 Url = "/record"
             });
 
@@ -40,12 +46,22 @@ namespace DPWH.EDMS.Web.Client.Pages.CurrentUser.Records
 
         protected async Task GetEmployeeRecords()
         {
-            var res = await LookupsService.GetPersonalRecords();
-            if (res.Success && res.Data != null)
-            {
-                var docId = Guid.Parse(Id);
-                EmployeeRecord = res.Data?.FirstOrDefault(x => x.Id == docId) ?? null;
+            ServiceCb = RecordManagementService.Query;
+            var filters = new List<Api.Contracts.Filter>();
+            AddTextSearchFilterIfNotNull(filters, nameof(LookupRecordModels.Id), Id, "eq");
+            SearchFilterRequest.Logic = DataSourceHelper.AND_LOGIC;
+            SearchFilterRequest.Filters = filters.Any() ? filters : null;
+            await LoadData();
+            var docId = Guid.Parse(Id);
+            //EmployeeRecord = res.Data?.FirstOrDefault(x => x.Id == docId) ?? null;
+            EmployeeRecord = GridData?.FirstOrDefault(x => x.Id == docId) ?? null;
 
+        }
+        private void AddTextSearchFilterIfNotNull(List<Api.Contracts.Filter> filters, string fieldName, string? value, string operation)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                AddTextSearchFilter(filters, fieldName, value, operation);
             }
         }
     }
