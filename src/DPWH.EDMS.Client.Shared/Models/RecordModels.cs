92878a5d2c2f3d04536b54b6e275a6d1ba4b2f45
Hennison <hennison.dee@blackfort.ph>
2024-08-06 10:47:06 +0800

    bracket fix

--- src/DPWH.EDMS.Client.Shared/Models/RecordModels.cs
+++ src/DPWH.EDMS.Client.Shared/Models/RecordModels.cs
@@ -2,20 +2,20 @@
 using Telerik.FontIcons;
 
 
-namespace DPWH.EDMS.Client.Shared.Models
-{
-    public class LookupRecordModels
-    {
-        public Guid Id { get; set; }
-        public string RecordName { get; set; }
-        public string EmployeeId { get; set; }
-        public Guid RecordTypeId { get; set; }
-        public string RecordUri { get; set; }
-        public string DocVersion { get; set; }
-        public List<RecordModels> Documents { get; set; }
+namespace DPWH.EDMS.Client.Shared.Models;
 
-    }
-    public class RecordModels{
+public class LookupRecordModels
+{
+    public Guid Id { get; set; }
+    public string RecordName { get; set; }
+    public string EmployeeId { get; set; }
+    public Guid RecordTypeId { get; set; }
+    public string RecordUri { get; set; }
+    public string DocVersion { get; set; }
+    public List<RecordModels> Documents { get; set; }
+
+}
+public class RecordModels{
     public Guid Id { get; set; }
     public Guid RecordTypeId { get; set; }
     public string EmployeeId { get; set; }
@@ -23,5 +23,5 @@ namespace DPWH.EDMS.Client.Shared.Models
     public string RecordUri { get; set; }
     public string DocVersion { get; set; }
 
-    }
 }
+
