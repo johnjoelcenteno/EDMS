fcd14c4271c3cb58b5c6f2e01b371c89d0d247e9
Joel John Centeno <joeljohn.centeno@blackfortconsulting.com>
2024-08-21 17:41:43 +0800

    Added object for proper client deserialization

---
+++ src/DPWH.EDMS.Application/Models/UserProfileDocuments/GetUserProfileDocumentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPWH.EDMS.Application.Models.UserProfileDocuments
+{
+    public class GetUserProfileDocumentModel
+    {
+        public Guid Id { get; set; }
+        public string EmployeeNumber { get; set; }
+        public string UriSignature { get; set; }
+    }
+}
