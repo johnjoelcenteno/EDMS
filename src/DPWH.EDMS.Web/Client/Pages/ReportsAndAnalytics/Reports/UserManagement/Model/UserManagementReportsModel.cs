e055d561466275903851d5d418ab7ad7894a2c23
Paulo Flores <pauloemmanuel.flores@blackfort.ph>
2024-09-13 11:31:08 +0800

    - 10870! - User Management Report (UI and Integration) - Temporary Used of UserService instead of ReportService (will revert back to original ReportService after BE fixed the endpoint)

---
+++ src/DPWH.EDMS.Web/Client/Pages/ReportsAndAnalytics/Reports/UserManagement/Model/UserManagementReportsModel.cs
@@ -0,0 +1,19 @@
+﻿namespace DPWH.EDMS.Web.Client.Pages.ReportsAndAnalytics.Reports.UserManagement.Model
+{
+    public class UserManagementReportsModel
+    {
+        public string? EmployeeId { get; set; }
+        public string? UserAccess { get; set; }
+        public string? Position { get; set; } // Added field
+        public string? Email { get; set; } // Added field
+        public string? SubOffice { get; set; } // Added field
+        public string? Office { get; set; } // Added field
+        public DateTimeOffset? CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string? FirstName { get; set; }
+        public string? MiddleInitial { get; set; }
+        public string? LastName { get; set; }
+        public string EmployeeFullName => $"{FirstName} {CheckMiddleInitial()} {LastName}";
+        public string CheckMiddleInitial() => !string.IsNullOrEmpty(MiddleInitial) ? $"{MiddleInitial}." : "";
+    }
+}
---
+++ src/DPWH.EDMS.Web/Client/Pages/ReportsAndAnalytics/Reports/UserManagement/UserManagementBase.cs
@@ -0,0 +1,179 @@
+﻿
+using DPWH.EDMS.Api.Contracts;
+using DPWH.EDMS.Client.Shared.APIClient.Services.Lookups;
+using DPWH.EDMS.Client.Shared.APIClient.Services.Reports;
+using DPWH.EDMS.Client.Shared.APIClient.Services.Users;
+using DPWH.EDMS.Client.Shared.Models;
+using DPWH.EDMS.Components.Components.ReusableGrid;
+using DPWH.EDMS.Components.Helpers;
+using DPWH.EDMS.IDP.Core.Constants;
+using DPWH.EDMS.Web.Client.Pages.ReportsAndAnalytics.Reports.UserManagement.Model;
+using DPWH.EDMS.Web.Client.Pages.ReportsAndAnalytics.Reports.Users.Model;
+using DPWH.EDMS.Web.Client.Shared.Services.ExceptionHandler;
+using DPWH.EDMS.Web.Client.Shared.Services.Export;
+using Microsoft.AspNetCore.Components;
+using Telerik.Blazor.Components;
+using Telerik.FontIcons;
+
+namespace DPWH.EDMS.Web.Client.Pages.ReportsAndAnalytics.Reports.UserManagement;
+//it should be UserManagementReportModel (Temporary fix) while waiting for BE fix
+public class UserManagementBase : GridBase<UserModel>
+{
+    #region
+    [Inject] public required ILookupsService LookUpService { get; set; }
+    [Inject] public required IExceptionHandlerService ExceptionHandlerService { get; set; }
+    [Inject] public required IReportsService ReportsService { get; set; }
+    [Inject] public required IExcelExportService ExcelExportService { get; set; }
+    [Inject] public required IUsersService UserService { get; set; }
+    #endregion
+
+    protected List<string> UserTypeList { get; set; } = new List<string>();
+    protected List<string> SelectedTypes { get; set; } = new List<string>();
+
+    protected readonly string TwoFormFieldsResponsiveClass = "col-12 col-md-6 col-lg-3";
+
+    protected string SelectedRegionalOffice { get; set; }
+    protected string SelectedDEO { get; set; }
+    protected List<GetRequestingOfficeResult> RequestingOffice = new List<GetRequestingOfficeResult>();
+    protected List<GetRequestingOfficeResultItem> DEOList { get; set; } = new List<GetRequestingOfficeResultItem>();
+    protected TelerikDropDownList<GetRequestingOfficeResult, string> ImplementDropRef = new();
+    protected bool Visible { get; set; } = false;
+    protected override async Task OnInitializedAsync()
+    {
+        base.OnInitialized();
+        await GetRequestingOffices();
+        BreadcrumbItems = new List<BreadcrumbModel>
+            {
+                new() { Icon = FontIcon.Home.ToString(), Url = "/" },
+                new() { Text = "Reports", Icon = FontIcon.Menu.ToString() },
+                new() { Text = "User", Icon = FontIcon.TableUnmerge.ToString() }
+            };
+
+        UserTypeList = ApplicationRoles.AssignableRoles.Select(ar => ar.Value).ToList();
+    }
+
+    protected async Task GetRequestingOffices()
+    {
+        var res = await ExceptionHandlerService.HandleApiException<GetRequestingOfficeResultIEnumerableBaseApiResponse>(async () => await LookUpService.GetRequestingOfficeList(), null);
+        if (res != null && res.Success)
+        {
+            RequestingOffice = res.Data.ToList();
+        }
+    }
+
+    protected async void OnChangeRegionOffice()
+    {
+        if (string.IsNullOrEmpty(SelectedRegionalOffice))
+        {
+            SelectedDEO = "";
+        }
+        if (SelectedRegionalOffice != null)
+        {
+
+            DEOList = RequestingOffice
+                    .Where(x => x.RegionName == SelectedRegionalOffice)
+                    .SelectMany(x => x.ImplementingOffices)
+                    .ToList();
+        }
+    }
+
+    protected async Task ShowReport(List<string> selectedTypes)
+    {
+        await ExceptionHandlerService.HandleApiException(async () =>
+        {
+            IsLoading = true;
+
+            //ServiceCb = ReportsService.QueryUser;
+            ServiceCb = UserService.Query;
+
+            var filters = GetFilters(selectedTypes, SelectedRegionalOffice, SelectedDEO);
+
+            SearchFilterRequest.Logic = DataSourceHelper.AND_LOGIC;
+            SearchFilterRequest.Filters = filters;
+            await LoadData();
+            if (string.IsNullOrEmpty(GridData?.FirstOrDefault()?.UserAccess))
+            {
+                ToastService.ShowError("No Data Found");
+            }
+            IsLoading = false;
+        });
+
+
+    }
+    private List<Filter> GetFilters(List<string> selectedTypes, string regionalOffice, string deo)
+    {
+        var filters = new List<Filter>();
+
+        if (selectedTypes != null)
+        {
+            var groupFilters = selectedTypes
+                .Select(item => CreateTextSearchFilter(nameof(UserModel.UserAccess), item, "contains"))
+                .ToList();
+
+            if (groupFilters.Count > 1)
+            {
+                filters.Add(new Filter
+                {
+                    Logic = DataSourceHelper.OR_LOGIC,
+                    Filters = groupFilters
+                });
+            }
+            else if (groupFilters.Count == 1)
+            {
+                filters.Add(groupFilters.Single());
+            }
+        }
+
+        if (!string.IsNullOrEmpty(regionalOffice))
+        {
+            filters.Add(CreateTextSearchFilter(nameof(UserModel.RegionalOfficeRegion), regionalOffice, "eq"));
+        }
+
+        if (!string.IsNullOrEmpty(deo))
+        {
+            filters.Add(CreateTextSearchFilter(nameof(UserModel.DistrictEngineeringOffice), deo, "eq"));
+        }
+
+        return filters;
+    }
+
+    private Filter CreateTextSearchFilter(string fieldName, string value, string comparison)
+    {
+        return new Filter
+        {
+            Field = fieldName,
+            Value = value,
+            Operator = comparison
+        };
+    }
+
+    protected async Task ConfirmToExcel()
+    {
+        IsLoading = true;
+        var fileName = $"User Report Type as of {DateTime.Now.ToString("MMM dd, yyyy")}.xlsx";
+        var items = await GetAllItems();
+
+        await ExceptionHandlerService.HandleApiException(async () => await ExcelExportService.ExportList(items, fileName), null);
+        //await _ExcelExportService.ExportList(items, fileName);
+
+        IsLoading = false;
+    }
+    protected async Task<List<UserModel>> GetAllItems()
+    {
+       //Temporary Fix - For Demo Purpose (will change to ReportService once BE Fix the endpoint
+
+       // var items = await GenericHelper.GetListByQuery<UserModel>(
+       //    new DataSourceRequest() { Skip = 0, Filter = DataSourceReq.Filter },
+       //    ReportsService.QueryUser,
+       //    (err) => ToastService.ShowError(err)
+       //);
+
+        var items = await GenericHelper.GetListByQuery<UserModel>(
+            new DataSourceRequest() { Skip = 0, Filter = DataSourceReq.Filter },
+            UserService.Query,
+            (err) => ToastService.ShowError(err)
+        );
+
+        return items;
+    }
+}
