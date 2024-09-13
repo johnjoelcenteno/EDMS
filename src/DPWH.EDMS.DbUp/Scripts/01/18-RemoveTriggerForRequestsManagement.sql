59fc0451920ae7d55a83c7a322d9882fd0206767
Joel John Centeno <joeljohn.centeno@blackfortconsulting.com>
2024-09-13 09:08:18 +0800

    Query the trigger then drop

--- src/DPWH.EDMS.DbUp/Scripts/01/18-RemoveTriggerForRequestsManagement.sql
+++ src/DPWH.EDMS.DbUp/Scripts/01/18-RemoveTriggerForRequestsManagement.sql
@@ -5,5 +5,18 @@ IF EXISTS (
 	INNER JOIN sysobjects AS so2 ON so.parent_obj = so2.Id
 	WHERE [so].[type] = 'TR'
 ) BEGIN 
-	DROP TRIGGER UpdateDateReleased
+	DECLARE @TriggerName NVARCHAR(100);
+
+
+	--DROP TRIGGER UpdateDateReleased
+	SELECT
+	@TriggerName = [so].name
+	FROM sysobjects AS [so]
+	INNER JOIN sysobjects AS so2 ON so.parent_obj = so2.Id
+	WHERE [so].[type] = 'TR' AND [so].[name] = 'UpdateDateReleased'
+
+	IF @TriggerName IS NOT NULL BEGIN
+		EXEC('DROP TRIGGER ' + @TriggerName)
+	END
+
 END;
