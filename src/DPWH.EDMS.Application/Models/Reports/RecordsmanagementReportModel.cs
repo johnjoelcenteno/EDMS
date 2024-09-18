81f71abc47dc4c612a3c7a020d14e63bf02bf2ac
Joel John Centeno <joeljohn.centeno@blackfortconsulting.com>
2024-09-18 20:16:38 +0800

    Created reports request management and fixed NoDays calculator in request management

---
+++ src/DPWH.EDMS.Application/Models/Reports/RecordsmanagementReportModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPWH.EDMS.Application.Models.Reports
+{
+    public class RecordsmanagementReportModel
+    {
+        public int Id { get; set; }
+        public DateTimeOffset? ReleaseDate { get; set; }
+        public string Status { get; set; }
+    }
+}
